10f2e1452589cb704c6a7c76f98e7395fa8cc4a7
jah3845 <120355426+jah3845@users.noreply.github.com>
2026-02-01 13:27:52 +0900

    Add additional URL keywords to config

--- scoring_system/config.py
+++ scoring_system/config.py
@@ -5,7 +5,9 @@ FAMILY_KEYWORDS = ["엄마", "아빠", "딸", "아들", "고장", "수리", "편
 AGENCY_KEYWORDS = ["검찰", "수사관", "서울지검", "금감원", "금융위원회", "계좌", "도용"]
 URGENCY_KEYWORDS = ["즉시", "마감", "당장", "긴급", "구속", "영장", "유포"]
 FINANCIAL_KEYWORDS = ["상품권", "핀번호", "송금", "이체", "대출", "승인", "선입금","전액",]
-URL_KEYWORDS = ["http", "https", ".com", ".kr", "bit.ly"]
+URL_KEYWORDS = ["http", "https", ".com", ".kr", "bit.ly",'click.gl', 'url.kr', 'band-us.tv', 'tr.im', 'vo.la',
+            'gg.gg', 'iii.im', 'open.kakao.com', 'band-us.io', 'han.gl',
+            'pf.kakao.com', 'na.to', 'vvd.bz', 'do.cco.kr', 'tuney.kr']
 
 # TF-IDF Settings
 TFIDF_MAX_FEATURES = 1000
